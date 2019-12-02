37498d389a86876569bb1ed83387610b69f02136
Keseru Szabolcs <keserusz94@gmail.com>
2019-12-02 21:10:22 +0200

    added CRUD endpoints for products

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -53,7 +53,7 @@ public class ProductService {
     public Page<Product> getProducts(GetProductsRequest request, Pageable pageable){
         LOGGER.info("Retrieving products: {}", request);
         if (request != null && request.getPartialName() != null && request.getMinQuantity() != null){
-            return productRepository.findByNameContainingAAndQuantityGreaterThanEqual(
+            return productRepository.findByNameContainingAndQuantityGreaterThanEqual(
                     request.getPartialName(), request.getMinQuantity(), pageable);
         }else if(request != null && request.getPartialName() != null){
             return productRepository.findByNameContaining(
